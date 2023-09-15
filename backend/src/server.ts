349c806c15170b707a3695e6742faf74f4f67e01
laura genari <lauragenari471@outlook.com>
2023-09-15 17:41:18 -0300

    checkpoint

---
+++ backend/src/server.ts
@@ -0,0 +1,13 @@
+//httpy pra req no terminal com carinha bonita
+import { fastify } from 'fastify';
+import { getAllPromptsRoute } from './routes/get-all-prompts';
+
+const app = fastify();
+
+app.register(getAllPromptsRoute)
+
+app.listen({
+    port: 3333,
+}).then(() => {
+    console.log('HTTP server running!')
+})
